20dfaada69ffdefb0c09f6abb4cf108ec103d448
haru <kawachigenji95@gmail.com>
2024-06-27 01:01:52 +0900

    Save in progress, bug that causes 504 error in nginx reverse proxy

--- backend/app/Helpers/SessionManager.php
+++ backend/app/Helpers/SessionManager.php
@@ -2,21 +2,11 @@
 
 namespace Helpers;
 
-use Settings\Settings;
-
 class SessionManager
 {
     public static function startSession(): void
     {
         if (session_status() === PHP_SESSION_NONE) {
-            // session_set_cookie_params([
-            //     'lifetime' => 0,
-            //     'path' => '/',
-            //     'domain' => Settings::env('SESSION_DOMAIN'),
-            //     'secure' => filter_var(getenv('FEATURE_ENABLED'), FILTER_VALIDATE_BOOLEAN),
-            //     'httponly' => true,
-            //     'samesite' => 'None'
-            // ]);
             session_start();
         }
     }
@@ -30,13 +20,6 @@ class SessionManager
     public static function get($key): mixed
     {
         self::startSession();
-
-        // TODO ITテスト後に削除する
-        // -----------------------------------------------
-        if ($key == 'user_id') return 1;
-        if ($key == 'user_name') return 'chisato';
-        // -----------------------------------------------
-
         return $_SESSION[$key] ?? null;
     }
 
