817a3ba05a192d2f2dad7e2d42ac969f8b114777
haru <kawachigenji95@gmail.com>
2024-07-01 00:56:18 +0900

    Save in progress, fixing mysql connection from php

--- backend/app/Database/MySQLWrapper.php
+++ backend/app/Database/MySQLWrapper.php
@@ -7,9 +7,16 @@ use Settings\Settings;
 
 class MySQLWrapper extends mysqli
 {
-    public function __construct(?string $hostname = 'localhost', ?string $username = null, ?string $password = null, ?string $database = null, ?int $port = null, ?string $socket = null)
-    {
+    public function __construct(
+        ?string $hostname = null,
+        ?string $username = null,
+        ?string $password = null,
+        ?string $database = null,
+        ?int $port = null,
+        ?string $socket = null
+    ) {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $hostname = $hostname ?? Settings::env('DATABASE_HOST');
         $username = $username ?? Settings::env('DATABASE_USER');
         $password = $password ?? Settings::env('DATABASE_USER_PASSWORD');
         $database = $database ?? Settings::env('DATABASE_NAME');
