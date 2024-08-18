64b51eee2a0f8b964209dd494d56e86d0ed1ea80
haru <kawachigenji95@gmail.com>
2024-08-18 22:03:40 +0900

    Update workflow

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const apiDomain = 'http://sns.test.com';
+// const apiDomain = 'http://sns.test.com';
+const apiDomain = 'http://sns.haru864.com';
 // const sseDomain = 'http://localhost:4001';
 const sseDomain = apiDomain;
 
 const nextConfig = {
     env: {
-        FRONT_DOMAIN: 'http://sns.test.com',
+        // FRONT_DOMAIN: 'http://sns.test.com',
+        FRONT_DOMAIN: 'http://sns.haru864.com',
         // FRONT_DOMAIN: 'http://localhost:3000',
         API_DOMAIN: apiDomain,
         PROFILE_IMAGE_THUMBNAIL_URL: `${apiDomain}/images/profile/thumbnail`,
